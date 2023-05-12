7fbe59fb65d5b8c80cb03cbd7b503509ca24a76b
ASUS <ms0565119@hotmail.com>
2023-05-12 11:16:20 +0800

    add Merge time

--- src/main/java/com/example/animal_adoption/AnimalAdoptionApplication.java
+++ src/main/java/com/example/animal_adoption/AnimalAdoptionApplication.java
@@ -8,6 +8,7 @@ public class AnimalAdoptionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AnimalAdoptionApplication.class, args);
+    // 梁 上午 5/12 11:16:00 Merge "feature_animal_adoption"
 	}
 
 }
