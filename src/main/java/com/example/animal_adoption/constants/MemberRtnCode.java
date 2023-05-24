ac311f8fd0b5fa67e8547fe3b1b0705466b404af
yiting <yiting50425@gmail.com>
2023-05-24 21:00:46 +0800

    update RtnCode

--- src/main/java/com/example/animal_adoption/constants/MemberRtnCode.java
+++ src/main/java/com/example/animal_adoption/constants/MemberRtnCode.java
@@ -3,7 +3,12 @@ package com.example.animal_adoption.constants;
 
 public enum MemberRtnCode {
 	
-	SUCCESSFUL("200", "成功");
+	SUCCESSFUL_LOG_IN("200", "登入成功"),
+	SUCCESSFUL_LOG_OUT("200", "登出成功"),
+	SUCCESSFUL_SIGN_UP("200", "註冊成功"),
+	SUCCESSFUL_ACTTIVE_ACCOUNT("200", "帳號生效成功"),
+	SUCCESSFUL_UPDATE_MEMBER_INFO("200", "修改會員資訊成功");
+	
 	
 	private String code;
 
