21bedf1b2b202921050f346c9f884b1a09d86a9f
DESKTOP-L10ENU7\a0920 <a0920360129@gmail.com>
2023-06-08 22:02:59 +0800

    0608Shane

---
+++ src/main/java/com/example/animal_adoption/vo/DeleteProductRequest.java
@@ -0,0 +1,72 @@
+package com.example.animal_adoption.vo;
+
+import com.example.animal_adoption.entity.Member;
+
+import java.util.Map;
+
+public class DeleteProductRequest {
+
+    private int carId;
+    private int productId;
+    private int price;
+    private int stock;
+    private int quantity;
+    private Map<Integer, Integer> carMap;
+    private Member member;
+
+    public int getCarId() {
+        return carId;
+    }
+
+    public void setCarId(int carId) {
+        this.carId = carId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public Map<Integer, Integer> getCarMap() {
+        return carMap;
+    }
+
+    public void setCarMap(Map<Integer, Integer> carMap) {
+        this.carMap = carMap;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
