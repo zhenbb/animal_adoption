2f30fce29d8f906432e719956796d60c1dd1c441
ASUS <ms0565119@hotmail.com>
2023-06-11 17:55:57 +0800

    mail init build.gradle add

---
+++ src/main/java/com/example/animal_adoption/entity/Mail.java
@@ -0,0 +1,62 @@
+package com.example.animal_adoption.entity;
+
+import jakarta.mail.*;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+
+import java.util.Properties;
+
+public class Mail {
+  private String userName = "ms0565119@gmail.com"; // 寄件者email
+
+  private String password = "ztzalkilgixjnksq"; // 收寄件者密碼
+
+  private String customer = "ms0565119@gmail.com"; // 收件者email
+
+  private String subject = "測試郵寄"; // 標題
+
+  private String txt = "Hey"; // 信件內容
+
+  public void SendMAil() {
+    Properties prop = new Properties();
+
+    prop.setProperty("mail.transport.protocol", "smtp");
+
+    prop.setProperty("mail.host", "smtp.gmail.com");
+
+    prop.put("mail.smtp.port", "465");
+
+    prop.put("mail.smtp.auth", "true");
+
+    prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+    prop.put("mail.smtp.socketFactory.port", "465");
+
+    Session session = Session.getDefaultInstance(prop, new Authenticator() {
+      @Override
+      protected PasswordAuthentication getPasswordAuthentication() {
+        return new PasswordAuthentication(userName, password);
+      }
+    });
+
+    Message message = new MimeMessage(session);
+
+    try {
+      message.setFrom(new InternetAddress(userName));
+
+      message.setRecipient(Message.RecipientType.TO, new InternetAddress(customer));
+
+      message.setSubject(subject);
+
+      message.setContent(txt,"text/html;charset=UTF-8");
+
+      // transport
+      Transport transport = session.getTransport();
+      Transport.send(message);
+      transport.close();
+
+    } catch (MessagingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
