5697c12993c0c2e0852039e12794aaea20e30708
ASUS <ms0565119@hotmail.com>
2023-06-12 16:14:26 +0800

    WebMvcConfig 修改

--- src/main/java/com/example/animal_adoption/config/WebMvcConfig.java
+++ src/main/java/com/example/animal_adoption/config/WebMvcConfig.java
@@ -2,15 +2,17 @@ package com.example.animal_adoption.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
 
+  @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
             .allowedOrigins("*")
             .allowCredentials(true)
-            .allowedMethods("POST")
+            .allowedMethods("POST","GET")
             .allowedHeaders("*");
   }
 }
