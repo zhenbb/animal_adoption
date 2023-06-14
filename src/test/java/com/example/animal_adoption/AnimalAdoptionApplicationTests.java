e5e9c74c6736893112d4469c83d0ebed3004d5c1
ASUS <ms0565119@hotmail.com>
2023-06-14 21:32:12 +0800

    fixed bug

--- src/test/java/com/example/animal_adoption/AnimalAdoptionApplicationTests.java
+++ src/test/java/com/example/animal_adoption/AnimalAdoptionApplicationTests.java
@@ -1,33 +1,8 @@
 package com.example.animal_adoption;
 
-<<<<<<< HEAD
-import java.util.List;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-=======
-import com.example.animal_adoption.util.Base64ToImg2;
-import org.junit.jupiter.api.Test;
->>>>>>> Base64ToImg_test
 import org.springframework.boot.test.context.SpringBootTest;
-
-import com.example.animal_adoption.entity.Product;
-import com.example.animal_adoption.repository.ProductDao;
 
 @SpringBootTest
 class AnimalAdoptionApplicationTests {
 
-<<<<<<< HEAD
-	@Autowired
-	ProductDao productDao;
-	
-	@Test
-	public void findAllProduct() {
-		List<Product> result = productDao.findAll();
-		System.out.println(result.size());
-	}
-=======
-
-
->>>>>>> Base64ToImg_test
 }
