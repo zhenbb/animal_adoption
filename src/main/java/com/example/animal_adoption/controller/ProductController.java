0eab033d74b5086641b4e1ec2cf552471df557c6
ThisIsNana <pw10279@gmail.com>
2023-06-15 17:54:08 +0800

    update product

--- src/main/java/com/example/animal_adoption/controller/ProductController.java
+++ src/main/java/com/example/animal_adoption/controller/ProductController.java
@@ -1,5 +1,7 @@
 package com.example.animal_adoption.controller;
 
+import java.util.Optional;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.animal_adoption.constants.MemberRtnCode;
 import com.example.animal_adoption.constants.RtnCode;
 import com.example.animal_adoption.constants.SessionCode;
+import com.example.animal_adoption.entity.Product;
 import com.example.animal_adoption.repository.ProductDao;
 import com.example.animal_adoption.service.ifs.ProductService;
 import com.example.animal_adoption.vo.ProductAddRequest;
@@ -33,60 +36,37 @@ public class ProductController {
 	// 新增商品
 //	@CrossOrigin(value="http://127.0.0.1:5501")
 	@PostMapping(value = "add_product")
-	public ProductResponse addProduct(@RequestBody ProductAddRequest request, HttpSession httpSession) {
-		
-		if(!checkLogin(httpSession)) {
-			return new ProductResponse(MemberRtnCode.NOT_LOG_IN.getMessage());
-		}
-		return productService.addProduct(request);
+	public ProductResponse addProduct(@RequestBody ProductAddRequest request,HttpSession httpSession) {
+		return productService.addProduct(request.getProductName(),request.getCategory(),request.getPrice(),request.getStock());
 	}
 
 	
 	
 	// 更新 1. 商品名稱
 	@PostMapping(value = "update_product_name")
-	public ProductResponse updateProductName(@RequestBody ProductUpdateRequest request, HttpSession httpSession) {
-		
-		if(!checkLogin(httpSession)) {
-			return new ProductResponse(MemberRtnCode.NOT_LOG_IN.getMessage());
-		}  
-
+	public ProductResponse updateProductName(@RequestBody ProductUpdateRequest request,HttpSession httpSession) {
 		return productService.updateProductName(request.getProductId(), request.getProductName());
 	}
 
 	
 	// 更新 2.分類
 	@PostMapping(value = "update_product_category")
-	public ProductResponse updateProductCategory(@RequestBody ProductUpdateRequest request,
-			HttpSession httpSession) {
-		if(!checkLogin(httpSession)) {
-			return new ProductResponse(MemberRtnCode.NOT_LOG_IN.getMessage());
-		}
-
+	public ProductResponse updateProductCategory(@RequestBody ProductUpdateRequest request,HttpSession httpSession) {
 		return productService.updateProductCategory(request.getProductId(), request.getCategory());
 	}
 
 	
 	// 更新 3. 庫存
 	@PostMapping(value = "update_product_stock")
-	public ProductResponse updateProductStcok(@RequestBody ProductUpdateRequest request,
-			HttpSession httpSession) {
-		if(!checkLogin(httpSession)) {
-			return new ProductResponse(MemberRtnCode.NOT_LOG_IN.getMessage());
-		}
+	public ProductResponse updateProductStcok(@RequestBody ProductUpdateRequest request,HttpSession httpSession) {
 
 		return productService.updateProductStock(request.getProductId(), request.getStock());
 	}
 
 	// 更新 4. 價格
 	@PostMapping(value = "update_product_price")
-	public ProductResponse updateProductPrice(@RequestBody ProductUpdateRequest request,
-			HttpSession httpSession) {
-		if(!checkLogin(httpSession)) {
-			return new ProductResponse(MemberRtnCode.NOT_LOG_IN.getMessage());
-		}
-
-		return productService.updateProductPrice(request.getProductId(), request.getPrice());
+	public ProductResponse updateProductPrice(@RequestBody ProductUpdateRequest request,HttpSession httpSession) {
+			return productService.updateProductPrice(request.getProductId(), request.getPrice());
 	}
 
 	// 關鍵字搜尋
@@ -99,19 +79,30 @@ public class ProductController {
 	//秀出所有商品
 	@GetMapping(value="find_all")
 	public ProductResponse findAll() {
-		return new ProductResponse( productDao.findAll(),RtnCode.PRODUCT_SEARCH_SUCCESS.getMessage());
+		return new ProductResponse(productDao.findAll(),RtnCode.PRODUCT_SEARCH_SUCCESS.getMessage());
 		
 	}
 
-	// session判斷是否有登入
-	private boolean checkLogin(HttpSession httpSession) {
-		String sessionMemberId = (String) httpSession.getAttribute(SessionCode.MEMBER_ID.getCode());
-		String sessionPwd = (String) httpSession.getAttribute(SessionCode.MEMBER_PWD.getCode());
-		
-	    if (!StringUtils.hasText(sessionMemberId) || !StringUtils.hasText(sessionPwd)) {
-	    	return false;
-	    }
-		return true;
-		
+	//前端用
+	//找一個商品
+	@PostMapping(value="find_one")
+	public ProductResponse findOne(@RequestBody int id) {
+		Optional<Product> op = productDao.findById(id);
+		if(!op.isPresent()) {
+			return new ProductResponse(RtnCode.PRODUCT_NOT_FOUND.getMessage());
+		}
+		return new ProductResponse(productDao.findById(id).get(),RtnCode.PRODUCT_SEARCH_SUCCESS.getMessage());
 	}
+	
+
+//	// session判斷是否有登入
+//	private boolean checkLogin(HttpSession httpSession) {
+//		String sessionMemberId = (String) httpSession.getAttribute(SessionCode.MEMBER_ID.getCode());
+//		String sessionPwd = (String) httpSession.getAttribute(SessionCode.MEMBER_PWD.getCode());
+//		
+//	    if (!StringUtils.hasText(sessionMemberId) || !StringUtils.hasText(sessionPwd)) {
+//	    	return false;
+//	    }
+//		return true;	
+//	}
 }
--- src/test/java/com/example/animal_adoption/AnimalAdoptionApplicationTests.java
+++ src/test/java/com/example/animal_adoption/AnimalAdoptionApplicationTests.java
@@ -1,14 +1,9 @@
 package com.example.animal_adoption;
 
-<<<<<<< HEAD
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-=======
-import com.example.animal_adoption.util.Base64ToImg2;
-import org.junit.jupiter.api.Test;
->>>>>>> Base64ToImg_test
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.animal_adoption.entity.Product;
@@ -17,7 +12,6 @@ import com.example.animal_adoption.repository.ProductDao;
 @SpringBootTest
 class AnimalAdoptionApplicationTests {
 
-<<<<<<< HEAD
 	@Autowired
 	ProductDao productDao;
 	
@@ -26,8 +20,4 @@ class AnimalAdoptionApplicationTests {
 		List<Product> result = productDao.findAll();
 		System.out.println(result.size());
 	}
-=======
-
-
->>>>>>> Base64ToImg_test
 }
